40c1fb3a473f8a751cdef118caee440111b8a927
Whitney Hunter <whitney@hunterfam.com>
2023-03-15 15:00:59 -0700

    PCC-1 Bootstrapped react library
    * PCC-1 Bootstrapped React project
    
    * PCC-1 Added typescript
    
    * PCC-1 Added Rollup
    
    * PCC-1 Added ability to publish to NPM
    
    * PCC-1 Added css and optimizations
    
    * PCC-1 Added jest for testing
    
    * PCC-1 Added storybook

---
+++ rollup.config.js
@@ -0,0 +1,57 @@
+/*
+ * Copyright 2023 Whitney Hunter
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import resolve from "@rollup/plugin-node-resolve"
+import commonjs from "@rollup/plugin-commonjs"
+import typescript from "@rollup/plugin-typescript"
+import dts from "rollup-plugin-dts"
+import postcss from "rollup-plugin-postcss"
+import peerDepsExternal from 'rollup-plugin-peer-deps-external';
+
+import terser from "@rollup/plugin-terser"
+import packageJson from './package.json' assert {type: 'json'}
+
+export default [
+    {
+        input: "src/index.ts",
+        output: [
+            {
+                file: packageJson.main,
+                format: "cjs",
+                sourcemap: true,
+            },
+            {
+                file: packageJson.module,
+                format: "esm",
+                sourcemap: true,
+            }
+        ],
+        plugins: [
+            peerDepsExternal(),
+            resolve(),
+            commonjs(),
+            typescript({tsconfig: "./tsconfig.json"}),
+            postcss(),
+            terser(),
+        ],
+    },
+    {
+        input: "dist/esm/types/index.d.ts",
+        output: [{file: "dist/index.d.ts", format: "esm"}],
+        plugins: [dts()],
+        external: [/\.css$/],
+    }
+]
